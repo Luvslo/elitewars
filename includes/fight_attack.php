62ccff36ad0d21fe36a468bb05a25028db343826
gratefuldeadty <gratefuldeadty@gmail.com>
2014-10-02 14:27:11 -0400

    Update fight_attack.php

--- includes/fight_attack.php
+++ includes/fight_attack.php
@@ -1,3 +1,30 @@
 <?php
 
+
+include '../classes/session.php';
+
+Session::init();
+
+include '../classes/users.php';
+include '../classes/attack.php';
+
+$userLoad = new Users(Database $dbh);
+
+$targetFetch = $userLoad->checkUsername($targetname);
+$targetid = $targetFetch['id'];
+$targetData = $userLoad->userData($targetid);
+
+$target_atk = $targetData['attack'];
+$target_hp = $targetData['hp'];
+$target_crit = $targetData['critical'];
+
+$user_atk = $userData['attack'];
+$user_hp = $userData['hp'];
+$user_crit = $userData['critical'];
+
+
+
+
+
+
 ?>
