1fae6e58155208a42f76f5a255ca9e069270749f
gratefuldeadty <gratefuldeadty@gmail.com>
2014-10-02 13:10:20 -0400

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+* Profile, using $_GET['userid'];
+*/
+
+include 'classes/session.php';
+include 'classes/database.php';
+include 'classes/users.php';
+
+Session::init();
+
+$userid = htmlentities((int)$_GET['userid']); //make sure the userid is an integer.
+
+$userLoad = new Users(Database $dbh);
+
+//check to see if the user exists.
+if ($userLoad->userData($userid) == false)
+{
+    $error = 'Error: This user does not exist.';
+    include_once 'includes/error.php';
+    exit;
+}
+else
+{
+    //user exists.
+    $userData = $userLoad->userData($userid);
+}
+
+$username = $userData['username'];
+
+echo 'Viewing the profile of '.$username;
+
+?>
