a62ac18149edcf6dd9180b4c5b394dbf9e7f1064
gratefuldeadty <gratefuldeadty@gmail.com>
2014-11-16 14:38:02 -0500

    Update quest.php

--- xml/quest.php
+++ xml/quest.php
@@ -1,4 +1,45 @@
 <?php
 
-  $mysongs = simplexml_load_file('songs.xml');
-    echo $mysongs->song[0]->artist;
+// Loading the quest xml file.
+// Debating on if this is going to be better than using a database..
+
+$dbh = new Database();
+
+$xmlQuests = simplexml_load_file('quests.xml');
+$dbQuests = new Quests($dbh);
+
+$questid = (int) $_GET['questid'];
+
+$userid = Session::get('userid'); 
+
+
+        /**
+         * Get the player quest
+         * @param int $questid
+         * @param int $userid
+         * @return bool : fetch
+         */
+        public function playerQuest($questid, $userid)
+        {
+            $query = $this->dbh->prepare('SELECT `step` FROM `playerquests` 
+                WHERE `questid` = ?
+                AND `userid` = ?');
+            $query->execute(array($questid, $userid));
+            
+            // false = no quest active
+            // true = fetches the results, as am object.
+            return ($query->rowCount() == 0) ? false : $query->fetch(PDO::FETCH_OBJ);
+       }
+
+
+$playerQuest = $dbQuests->playerQuest($questid, $userid);
+$step = $playerQuest->step;
+
+if ($playerQuest->completed == 1)
+{
+    echo $xmlQuests->quests['questid']->completed;
+}
+else
+{
+    echo $xmlQuests->quests['questid']->step . $step;
+}
